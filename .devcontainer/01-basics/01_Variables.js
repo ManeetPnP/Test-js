2aee9c4fd40467c7c660762958f256435947a60f
ManeetPnP <maneetghangas@gmail.com>
2024-06-19 10:24:44 +0000

    DataTypes

---
+++ .devcontainer/01-basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = "145632"
+
+let accountName = "abcd"
+
+
+// prefer not to use var 
+console.table([accountId, accountName])
---
+++ .devcontainer/01-basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use Strict"; // treat all js code as newer version 
+
+// code readability should be high
+
+let name = "maneet"
+let age = 27
+let isloggedIn = false
+
+console.log(typeof age)
+console.log(typeof "maneet")
+console.log(typeof null)
+console.log(typeof undefined)
